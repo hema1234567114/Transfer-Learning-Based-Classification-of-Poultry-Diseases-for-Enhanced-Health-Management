e2a9854af349b054a7c8757711499106636c907c
hema1234567114 <hemuh868@gmail.com>
2025-06-30 16:06:17 +0530

    Create Code.py

---
+++ Code/Code.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout
+from tensorflow.keras.optimizers import Adam
+import matplotlib.pyplot as plt
+import os
